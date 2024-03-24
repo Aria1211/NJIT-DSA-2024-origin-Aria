1070dfd602fcfd38a78529740f4ba47b547bb23b
Aria1211 <liu15863347229@163.com>
2024-03-24 16:59:10 +0800

    Submit04-1

--- 04-phonebook/src/main/java/oy/tol/tra/Person.java
+++ 04-phonebook/src/main/java/oy/tol/tra/Person.java
@@ -8,7 +8,7 @@ public class Person implements Comparable<Person> {
         this.firstName = new String(person.firstName);
         this.lastName = new String(person.lastName);
     }
-    
+
     public Person(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
@@ -32,22 +32,21 @@ public class Person implements Comparable<Person> {
     }
 
     /**
-     * TODO: Implement the method below to return a hash value. It must be calculated from the
      * first and last name of the person.
-     * 
+     *
      * @return Hash value of the person.
      */
     @Override
     public int hashCode() {
-        int hash = 5381;
-        // Implement hash function here.
-        return hash;
+        return getFullName().hashCode();
     }
 
     @Override
     public boolean equals(Object other) {
         if (other instanceof Person) {
-            return this.getFullName().equals(((Person)other).getFullName());
+            if (other.hashCode() == this.hashCode()) {
+                return this.getFullName().equals(((Person) other).getFullName());
+            }
         }
         return false;
     }
@@ -63,6 +62,7 @@ public class Person implements Comparable<Person> {
      * Return 0 if the full name is identical.
      * Return >0 if the other persons full name is larger (as string).
      * Note: String class also implements <code>compareTo()</code> you can use here.
+     *
      * @returns Returns 0 if persons are the same otherwise depending on the full name, <0 or >0.
      */
     @Override
